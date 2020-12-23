b1fa89c18636b0d2cfa79f2775f3285f10c7e8cb
mosquid <mosquid@gmail.com>
2020-12-23 11:53:46 +0200

    site distro added

--- lib/verifier_service.ts
+++ lib/verifier_service.ts
@@ -3,6 +3,10 @@ import * as apigateway from "@aws-cdk/aws-apigateway"
 import * as lambda from "@aws-cdk/aws-lambda"
 import * as dotenv from "dotenv"
 import * as dynamodb from "@aws-cdk/aws-dynamodb"
+import * as s3 from "@aws-cdk/aws-s3"
+import * as s3deploy from "@aws-cdk/aws-s3-deployment"
+import { BucketAccessControl } from "@aws-cdk/aws-s3"
+import * as cloudfront from "@aws-cdk/aws-cloudfront"
 
 dotenv.config()
 
@@ -13,6 +17,34 @@ export class VerifierService extends core.Construct {
     const table = new dynamodb.Table(this, "Subs", {
       partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    })
+
+    const bucket = new s3.Bucket(this, "caaspi-landing", {
+      accessControl: BucketAccessControl.PUBLIC_READ,
+      bucketName: "caaspi-lp",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+    })
+
+    const siteDistr = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "lp-distr",
+      {
+        originConfigs: [
+          {
+            behaviors: [{ isDefaultBehavior: true }],
+            s3OriginSource: {
+              s3BucketSource: bucket,
+            },
+          },
+        ],
+      }
+    )
+
+    new s3deploy.BucketDeployment(this, "HTML Bucket", {
+      sources: [s3deploy.Source.asset("./website")],
+      destinationBucket: bucket,
+      distribution: siteDistr
     })
 
     const handler = new lambda.Function(this, "WidgetHandler", {
